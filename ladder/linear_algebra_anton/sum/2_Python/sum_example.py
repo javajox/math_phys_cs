f9d981509a3fa152026f6c671878976a9bc8cb7e
javajox <per.neuron@gmail.com>
2026-02-13 15:41:30 +0200

    Example how to add two matrices in Python, using numpy

---
+++ ladder/linear_algebra_anton/sum/2_Python/sum_example.py
@@ -0,0 +1,20 @@
+# Example how to use numpy in Python for basic matrix operations
+
+import numpy as np
+
+# Create a 2x2 matrix
+A = np.array([[1, 2], 
+              [3, 4]])
+
+# Create a 2x2 matrix
+B = np.array([[5, 6], 
+              [7, 8]])
+
+# Addition
+C = A + B
+
+# Subtraction
+D = A - B
+
+# Scalar Multiplication (scaling a matrix)
+E = 2 * A
